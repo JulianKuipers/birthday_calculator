f6ee354add3e9b9b33896aa3bf4f7c2505729b62
JulianKuipers <34219530+JulianKuipers@users.noreply.github.com>
2020-07-13 10:26:28 +0200

    Update ageCalc.js

--- ageCalc.js
+++ ageCalc.js
@@ -1,18 +1,12 @@
-"use strict";
 function ageCalc(dob, mob, yob) {
-    var d = new Date();
-    var day = d.getDate();
-    var month = d.getMonth();
-    var year = d.getFullYear();
+    const TODAY = new Date();
+    const DAY = TODAY.getDate();
+    const MONTH = TODAY.getMonth();
+    const YEAR = TODAY.getFullYear();
 
-    var age;
+    let age = YEAR - yob;
 
-    if (month >= (mob - 1) && day >= (dob - 1)) {
-        age = year - yob;
-    }
-    else {
-        age = year - yob - 1;
-    }
+    age = (MONTH >= (mob - 1) && DAY >= (dob)) ? age : age - 1;
 
     document.getElementById("ageCalcOutput").innerHTML = age + " years old";
 }
